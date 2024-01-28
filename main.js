9a9833766acc3c788c33209fdb4d093493bf5f48
LuChao <atorber@163.com>
2024-01-29 00:00:08 +0800

    0.2.0
    增加机器人代码

--- main.js
+++ main.js
@@ -1,10 +1,65 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { spawn, exec } = require('child_process');
 const path = require('path');
+const {
+    WechatyBuilder,
+    log,
+} = require('wechaty')
+const { FileBox } = require('file-box')
+const { PuppetBridge } = require('wechaty-puppet-bridge')
 
 let mainWindow;
 let sidecarProcess = null;
 let funtoolProcess = null;
+let result = '程序启动，载入中...';
+
+async function onLogin(user) {
+    log.info('onLogin', '%s login', user)
+    result = `${user} login<br>` + result;
+    mainWindow.webContents.send('action-result', result);
+
+    const roomList = await bot.Room.findAll()
+    console.info('room count:', roomList.length)
+    const contactList = await bot.Contact.findAll()
+    console.info('contact count:', contactList.length)
+    result = `contact count: ${contactList.length},room count: ${roomList.length}<br>` + result;
+    mainWindow.webContents.send('action-result', result);
+
+}
+
+async function onMessage(message) {
+    log.info('onMessage', JSON.stringify(message))
+    const text = message.text()
+    const room = message.room()
+    const talker = message.talker()
+    // 生成2024-1-28 21:51:06格式的时间
+    const timeutc = new Date().toLocaleString()
+    result = `${timeutc} ${room ? '[' + await room.topic() + ']' : ''} ${talker.name()}: ${text}<br>` + result
+    mainWindow.webContents.send('action-result', result); 
+    // 1. send Image
+    if (/^ding$/i.test(message.text())) {
+        const fileBox = FileBox.fromUrl('https://wechaty.github.io/wechaty/images/bot-qr-code.png')
+        await message.say(fileBox)
+    }
+
+    // 2. send Text
+
+    if (/^dong$/i.test(message.text())) {
+        await message.say('dingdingding')
+    }
+
+}
+
+const puppet = new PuppetBridge({
+    nickName: '大师'  // 登录微信的昵称
+})
+const bot = WechatyBuilder.build({
+    name: 'ding-dong-bot',
+    puppet,
+})
+
+bot.on('login', onLogin)
+bot.on('message', onMessage)
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -22,62 +77,76 @@ function createWindow() {
     mainWindow.loadFile('index.html');
 
     ipcMain.on('start-funtool', () => {
-        const timeutc = new Date().toLocaleString('zh-CN', { hour12: false }).replace(/\//g, '-').replace(/:/g, '-').replace(/ /g, '-');
+        const timeutc = new Date().toLocaleString();
         if (!funtoolProcess) {
             const execPath = path.join(__dirname, 'assets', 'funtool_wx=3.9.2.23.exe');
             funtoolProcess = spawn(execPath);
             funtoolProcess.on('spawn', () => {
-                mainWindow.webContents.send('action-result', timeutc + ':funtool已启动！');
+                result = `${timeutc}:funtool已启动！<br>` + result;
+                mainWindow.webContents.send('action-result', result);
             });
             funtoolProcess.on('error', (err) => {
-                mainWindow.webContents.send('action-result', timeutc + ':启动funtool时发生错误: ' + err);
+                result = `${timeutc}:启动funtool时发生错误: ${err}<br>` + result;
+                mainWindow.webContents.send('action-result', result);
             });
         } else {
-            mainWindow.webContents.send('action-result', timeutc + ':funtool已在运行中...');
+            result = `${timeutc}:funtool已在运行中...<br>` + result;
+            mainWindow.webContents.send('action-result', result);
         }
     });
 
     ipcMain.on('stop-funtool', () => {
-        const timeutc = new Date().toLocaleString('zh-CN', { hour12: false }).replace(/\//g, '-').replace(/:/g, '-').replace(/ /g, '-');
+        const timeutc = new Date().toLocaleString();
 
         if (funtoolProcess) {
             funtoolProcess.kill();
             funtoolProcess = null;
-            mainWindow.webContents.send('action-result', timeutc + ':funtool已停止。');
+            result = `${timeutc}:funtool已停止...<br>` + result;
+            mainWindow.webContents.send('action-result', result);
         } else {
-            mainWindow.webContents.send('action-result', timeutc + ':funtool未在运行...');
+            result = `${timeutc}:funtool未在运行...<br>` + result;
+            mainWindow.webContents.send('action-result', result);
         }
     });
 
     ipcMain.on('start-sidecar', () => {
-        const timeutc = new Date().toLocaleString('zh-CN', { hour12: false }).replace(/\//g, '-').replace(/:/g, '-').replace(/ /g, '-');
+        const timeutc = new Date().toLocaleString();
 
         if (!sidecarProcess) {
-            const execPath = path.join(__dirname, 'assets','wxbot-sidecar.exe');
+            const execPath = path.join(__dirname, 'assets', 'wxbot-sidecar.exe');
             sidecarProcess = spawn(execPath);
             sidecarProcess.on('spawn', () => {
-                mainWindow.webContents.send('action-result', timeutc + ':Sidecar已启动！');
+                result = `${timeutc}:Sidecar已启动！<br>` + result;
+                mainWindow.webContents.send('action-result', result);
             });
             sidecarProcess.on('error', (err) => {
-                mainWindow.webContents.send('action-result', timeutc + ':启动Sidecar时发生错误: ' + err);
+                result = `${timeutc}:启动Sidecar时发生错误: ${err}<br>` + result;
+                mainWindow.webContents.send('action-result', result);
             });
         } else {
-            mainWindow.webContents.send('action-result', timeutc + 'Sidecar已在运行中...');
+            result = `${timeutc}:Sidecar已在运行中...<br>` + result;
+            mainWindow.webContents.send('action-result', result);
         }
     });
 
     ipcMain.on('stop-sidecar', () => {
-        const timeutc = new Date().toLocaleString('zh-CN', { hour12: false }).replace(/\//g, '-').replace(/:/g, '-').replace(/ /g, '-');
-
+        const timeutc = new Date().toLocaleString();
         if (sidecarProcess) {
             sidecarProcess.kill();
             sidecarProcess = null;
-            mainWindow.webContents.send('action-result', timeutc + ':Sidecar已停止!');
+            result = `${timeutc}:Sidecar已停止...<br>` + result;
+            mainWindow.webContents.send('action-result', result);
         } else {
-            mainWindow.webContents.send('action-result', timeutc + ':Sidecar未在运行...');
+            result = `${timeutc}:Sidecar未在运行...<br>` + result;
+            mainWindow.webContents.send('action-result', result);
         }
     });
 
+    bot.start()
+    .then(() => {
+        return log.info('StarterBot', 'Starter Bot Started.')
+    })
+    .catch(console.error)
 }
 
 function checkWeChat() {
@@ -85,11 +154,11 @@ function checkWeChat() {
     const checkWeChatCommand = 'tasklist | findstr WeChat.exe';
 
     exec(checkWeChatCommand, (err, stdout, stderr) => {
-        let result;
+        let resultVersion = '';
         if (err || stderr) {
-            result = '无法检查 WeChat 状态。';
+            resultVersion = '无法检查 WeChat 状态...';
         } else if (stdout) {
-            result = 'WeChat 正在运行。';
+            resultVersion = 'WeChat 正在运行...';
             // 这里可以添加获取 WeChat 版本的代码
             const filePath = 'C:\\Program Files (x86)\\Tencent\\WeChat\\WeChat.exe';
             const command = `(Get-Item '${filePath}').VersionInfo | Select-Object -ExpandProperty ProductVersion`;
@@ -104,15 +173,15 @@ function checkWeChat() {
                     return;
                 }
 
-                result = `本机安装的版本号: ${stdout.trim()}`;
+                resultVersion = `本机安装的版本号: ${stdout.trim()}`;
                 console.log(`WeChat Version: ${stdout.trim()}`);
-                mainWindow.webContents.send('wechat-check-result', result);
+                mainWindow.webContents.send('wechat-check-result', resultVersion);
             });
         } else {
-            result = 'WeChat 未运行。';
+            resultVersion = 'WeChat 未运行...';
         }
         // 将检查结果发送到渲染进程
-        mainWindow.webContents.send('wechat-check-result', result);
+        mainWindow.webContents.send('wechat-check-result', resultVersion);
     });
 }
 
